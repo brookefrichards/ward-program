d80cb40d7447468c8e9b324b6395df6e8449656f
nicholas richards <vrm.richards@gmail.com>
2020-06-10 21:48:30 -0600

    Update server.

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ app.get('/', function (req, res) {
     res.send('Hello World')
 })
    
-app.listen(3000)
+var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+})
