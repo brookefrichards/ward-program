a5e6252c0435e081faa48ac8d2e904760fd4746c
Nr1cha <vrm.richards@gmail.com>
2020-06-11 00:07:45 -0600

    adding an array

--- ward-program-data.js
+++ ward-program-data.js
@@ -2,7 +2,16 @@
 
 const getData = function (req, res) {
     res.json({
-        name: 'Brooke Frandsen'
+        name: 'Brooke Frandsen',
+        "presenting": "test",
+        "conducting": "",
+        "chorister": "",
+        "organist": "",
+        "openingHymn": "",
+        "invocation": "",
+        "sacramentHymn": "",
+        "closingHymn": "",
+        "benediction": "",
     })
 }
 
