61e6d96ea368b921a1b275c0ddeeef46365a2d66
Nr1cha <vrm.richards@gmail.com>
2020-06-10 22:34:39 -0600

    set things up for the first time

--- server.js
+++ server.js
@@ -2,9 +2,18 @@
 
 const express = require('express');
 const app = express()
-
+const path = require("path")
+app.get("/ward-program", function (req, res) {
+    res.sendFile(__dirname+"/index.html")
+})
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
    
 app.listen(3000)
+
+
+
