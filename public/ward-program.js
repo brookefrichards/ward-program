e313680af88c75f6a6443a63c927a1ad85973275
Nr1cha <vrm.richards@gmail.com>
2020-06-15 16:57:42 -0600

    added comments to allow me to read js better for functions

--- public/ward-program.js
+++ public/ward-program.js
@@ -17,6 +17,8 @@ function readJson() {
         })
 }
 
+// different functions that are used
+// STORED FUNCTIONS BUCKET
 function addList(idName, thing, ThirdThing) {
     document.getElementById(idName).innerHTML = "test "+ "thing" + " "+ thing + " "+ ThirdThing
 }
